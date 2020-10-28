733e2d908b64dc543e11ccf6b846c9638c944d4e
Ashish Gautam <ashish.gm74@gmail.com>
2020-10-28 19:29:46 +0545

    updated chip

--- src/components/Quiz/QuizChip.js
+++ src/components/Quiz/QuizChip.js
@@ -1,22 +1,22 @@
-import React from "react";
-import { StyleSheet } from "react-native";
-import { Chip, Text } from "react-native-paper";
-import { DEFAULT_COLOR } from "../../utils/globalColors";
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {Chip, Text} from 'react-native-paper';
+import {DEFAULT_COLOR} from '../../utils/globalColors';
 
 const style = StyleSheet.create({
   chip: {
-    alignSelf: "center",
+    alignSelf: 'center',
     marginTop: 60,
     borderColor: DEFAULT_COLOR,
-    borderWidth: 1
+    borderWidth: 1,
   },
   text: {
-    color: DEFAULT_COLOR,
-    fontSize: 18
-  }
+    color: 'black',
+    fontSize: 18,
+  },
 });
 
-export default props => (
+export default (props) => (
   <Chip mode="outlined" style={style.chip}>
     <Text style={style.text}>{props.text}</Text>
   </Chip>
